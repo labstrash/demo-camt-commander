d0a873f0fa33a787755744c08fa751d7d6486029
vikunalabs <vikunalabs>
2026-07-28 22:34:57 +0200

    feat: initialize project

---
+++ src/main/java/com/example/commander/DemoCamtCommanderApplication.java
@@ -0,0 +1,13 @@
+package com.example.commander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCamtCommanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCamtCommanderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commander/DemoCamtCommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCamtCommanderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
